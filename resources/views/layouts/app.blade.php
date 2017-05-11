9acb42a9e72cae7b33aaf4844ea7e718d0d20b5a
likaSKH <lika.sikharulia@gmail.com>
2017-05-11 12:35:36 +0400

    initial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,7 @@
                         <span class="icon-bar"></span>
                     </button>
 
+
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
